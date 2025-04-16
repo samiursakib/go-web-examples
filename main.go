a6a7dcbc2fa76bf2ef4f60024048bf2e8654c7ea
samiursakib <rafisamiur@gmail.com>
2025-04-16 23:16:10 +0600

    serving static assets

--- main.go
+++ main.go
@@ -9,5 +9,8 @@ func main() {
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "requested path: %s", r.URL.Path)
 	})
+
+	fs := http.FileServer(http.Dir("assets/"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 	http.ListenAndServe(":8080", nil)
 }
