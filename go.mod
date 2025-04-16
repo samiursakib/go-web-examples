12eb6d4ae8047f527ae9dfaec575fd34f87f3a8e
samiursakib <rafisamiur@gmail.com>
2025-04-16 23:58:30 +0600

    gorilla/mux setup done and path params read on request

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-web-examples
 
 go 1.24.0
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -3,14 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "requested path: %s", r.URL.Path)
 	})
 
+	r.HandleFunc("/books/{title}/{page}", func (w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		fmt.Println("title:", vars["title"], "page", vars["page"])
+		fmt.Fprintf(w, "title: %s, page: %s", vars["title"], vars["page"])
+	})
 	fs := http.FileServer(http.Dir("assets/"))
 	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", r)
 }
