7cfeac12bc12f4c450a5e781fcc23e049cc53eeb
samiursakib <rafisamiur@gmail.com>
2025-04-25 05:15:46 +0600

    mysql database

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module go-web-examples
 
 go 1.24.0
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require github.com/gorilla/mux v1.8.1
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.9.2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+type User struct {
+	id int64
+	username string
+	password string
+	createdAt time.Time
+}
 
 func main() {
 	r := mux.NewRouter()
@@ -21,5 +35,84 @@ func main() {
 	})
 	fs := http.FileServer(http.Dir("assets/"))
 	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading env file: %s", err)
+	} else {
+		fmt.Println("loading env variables")
+	}
+
+	uri := fmt.Sprintf("%s:%s@(%s:%s)/%s?parseTime=true", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+	fmt.Println(uri)
+	db, err := sql.Open("mysql", uri)
+	if (err != nil) {
+		fmt.Println("error occured during opening database connection", err)
+	}
+
+	// uname, pass := "sakib", "sakib123"
+	// lastIndex, err := createUser(db, uname, pass)
+	// fmt.Println("last inserted id:", lastIndex)
+
+	// users, err := getAllUsers(db)
+	// fmt.Println(users)
+
+	if lastUser, ok := getLastUser(db); ok {
+		fmt.Println("last user:", lastUser)
+	} else {
+		fmt.Println("last user not found")
+	}
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("$", err)
+	}
 	http.ListenAndServe(":8080", r)
+}
+
+
+
+
+
+func getAllUsers(db *sql.DB) (users []User, err error) {
+	selectQuery := `SELECT * FROM users`
+	rows, err := db.Query(selectQuery)
+	if (err != nil) {
+		fmt.Println("error occured during query")
+		return nil, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var u User
+		err := rows.Scan(&u.id, &u.username, &u.password, &u.createdAt)
+		if (err != nil) {
+			fmt.Println("error occured in each element", err)
+			return nil, err
+		}
+		users = append(users, u)
+	}
+	return users, nil
+}
+
+func createUser(db *sql.DB, uname string, pass string) (id int64, err error) {
+	insertQuery := `INSERT INTO users(username, password) VALUES(?, ?)`
+	result, err := db.Exec(insertQuery, uname, pass)
+	if (err != nil) {
+		fmt.Println("error occured while creating user", err)
+		return -1, err
+	}
+	lastIndex, err := result.LastInsertId()
+	if (err != nil) {
+		return -1, err
+	}
+	return lastIndex, nil
+}
+
+func getLastUser(db *sql.DB) (u User, ok bool) {
+	findLastUserQuery := `SELECT * FROM users ORDER BY id DESC LIMIT 1`
+	err := db.QueryRow(findLastUserQuery).Scan(&u.id, &u.username, &u.password, &u.createdAt)
+	if (err != nil) {
+		return u, false
+	}
+	return u, true
 }
